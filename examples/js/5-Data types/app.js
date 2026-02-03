cb5f82d751b15b9ee26a57a9e3dafe595931cc47
Sergiu Chilat <chilatsergiu@gmail.com>
2026-02-03 08:35:24 +0200

    Data types

---
+++ examples/js/5-Data types/app.js
@@ -0,0 +1,14 @@
+let number = 2;
+let string = "Hello";
+let boolean = true;
+let obj = { name: "John", age: 30 };
+let bigNumBigint = 9007199254740991575877876877n;
+let bigNumInt = 9007199254740991575877876877;
+
+
+console.log("Number:", number);
+console.log("String:", string);
+console.log("Boolean:", boolean);
+console.log("Object:", obj);
+console.log("BigInt:", bigNumBigint);
+console.log("Number (large):", bigNumInt);
