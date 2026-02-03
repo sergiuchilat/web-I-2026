bf4f468c104d60acf14d34267f40a81f5f244e3c
Sergiu Chilat <chilatsergiu@gmail.com>
2026-02-03 08:57:25 +0200

    Operators

---
+++ examples/js/7-Operators/app.js
@@ -0,0 +1,29 @@
+let x = 2;
+const y = 3;
+
+console.log("Addition:", x + y);           // 5
+console.log("Subtraction:", x - y);        // -1
+console.log("Multiplication:", x * y);     // 6
+console.log("Division:", x / y);           // 0.666...
+console.log("Modulus:", x % y);            // 2
+console.log("Exponentiation:", x ** y);    // 8
+
+x++;
+
+const booleanValue = true;
+console.log("Negation:", !booleanValue);   // false
+
+const a = 5;
+const b = '5';
+
+console.log('a+b', a + b);
+
+const c = 4;
+const d = 8;
+
+console.log('c+d', c + d);
+
+const e = 2;
+const f = {props1: 1, props2: 2};
+
+console.log('e+f', e + f);
